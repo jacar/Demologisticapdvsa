10c39a095824a2058b1895e1764b46c456b1ba3e
[dyad] <git@dyad.sh>
2025-09-04 21:38:03 -0500

    Init CodeX app

---
+++ src/components/made-with-vibestacks.tsx
@@ -0,0 +1,14 @@
+export const MadeWithVibeStacks = () => {
+  return (
+    <div className="p-4 text-center">
+      <a
+        href="https://vibestacks.meegavault.in/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+      >
+        Made with VibeStacks Builder
+      </a>
+    </div>
+  );
+};
